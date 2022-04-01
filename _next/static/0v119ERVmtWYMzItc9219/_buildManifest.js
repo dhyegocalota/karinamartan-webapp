e5ebf8d9a3453422ec77246349ce4afea7c63328
dhyegocalota <dhyegofernando@gmail.com>
2022-04-01 22:58:19 +0000

    Deploying to gh-pages from @ dhyegocalota/karinamartan-webapp@4549213e791f3082021ecca69bd2faf346d70300 🚀

--- _next/static/0v119ERVmtWYMzItc9219/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dbee5beae5cafe49.css","static/chunks/pages/index-c52ce2b845ca365c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/HRc8d78t2wVQB_lhpvg4Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dbee5beae5cafe49.css","static/chunks/pages/index-e52983aec855fd00.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
