1750cffa37b92182a64f8825e8b627f74171f15d
dhyegocalota <dhyegofernando@gmail.com>
2022-04-03 00:45:23 +0000

    Deploying to gh-pages from @ dhyegocalota/karinamartan-webapp@968c3af8187fc6236a74b0abc3ae8067fdc28d14 🚀

---
+++ _next/static/5N9KFHZ26upcyecZqEuIX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e51af722f2960c92.css","static/chunks/pages/index-f105e8898c112a93.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
